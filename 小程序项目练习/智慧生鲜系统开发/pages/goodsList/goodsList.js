7266b5f56410a446b2ac4d3886400b32b592ca00
wnn0809 <w1418899532@163.com>
2019-06-19 17:44:49 +0800

    JJ生鲜

---
+++ 小程序项目练习/智慧生鲜系统开发/pages/goodsList/goodsList.js
@@ -0,0 +1,106 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    goodslist: [
+      {
+        "id": "001",
+        "name": "泰国龙眼500g*1盒【新人特惠】",
+        "desc": "在你的眼里，有我才甜蜜",
+        "price": "￥9.9",
+        "type": "500g/盒",
+        "pic_url": "http://img14.yiguoimg.com/e/ad/2016/160914/585749449477366062_260x320.jpg"
+      },
+      {
+        "id": "002",
+        "name": "伊利畅轻燕麦+草莓风味乳",
+        "desc": "这杯酸奶太有料了，要嚼着吃",
+        "price": "￥7.8",
+        "type": "300ml/杯",
+        "pic_url": "http://img14.yiguoimg.com/e/ad/2016/160914/585749449477366062_260x320.jpg"
+      },
+      {
+        "id": "003",
+        "name": "进口香蕉每袋500g起",
+        "desc": "跟鲜花一起运过来的香蕉",
+        "price": "￥6.8",
+        "type": "500g/袋",
+        "pic_url": "http://img14.yiguoimg.com/e/ad/2016/160914/585749449477366062_260x320.jpg"
+      },
+      {
+        "id": "004",
+        "name": "新疆吐鲁番哈密瓜1个250g起",
+        "desc": "咬一口吐鲁番风情的甜脆",
+        "price": "￥19.8",
+        "type": "250g/个",
+        "pic_url": "http://img14.yiguoimg.com/e/ad/2016/160914/585749449477366062_260x320.jpg"
+      },
+      {
+        "id": "005",
+        "name": "8424西瓜大个儿，一个5斤起",
+        "desc": "夏季清凉密码=8424",
+        "price": "￥26.9",
+        "type": "500g/个",
+        "pic_url": "http://img14.yiguoimg.com/e/ad/2016/160914/585749449477366062_260x320.jpg"
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
--- 小程序项目练习/智慧生鲜系统开发/pages/order/order.js
+++ 小程序项目练习/智慧生鲜系统开发/pages/order/order.js
@@ -4,13 +4,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    value: ''
+    value: '',
+    show:false
   },
   onChange(event) {
     // event.detail 为当前输入的值
     // console.log(event.detail);
   },
+  
+  
   fromsubmit: function (e){
-    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    console.log('form发生了submit事件，携带数据为：', e.detail.value);
+    
   }
 })
