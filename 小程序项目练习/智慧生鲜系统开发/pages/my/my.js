014648da770408e7ce298df444afb79b6983f514
wnn0809 <w1418899532@163.com>
2019-06-22 19:11:12 +0800

    JJ生鲜

--- 小程序项目练习/智慧生鲜系统开发/pages/my/my.js
+++ 小程序项目练习/智慧生鲜系统开发/pages/my/my.js
@@ -4,7 +4,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    userinfolist: {},
+    flag:true
+    // canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   /**
@@ -14,52 +16,27 @@ Page({
     
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  login: function() {
+    var self = this
+    // console.log(canIUse)
+    // 查看是否授权
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              self.setData({
+                userinfolist: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+    this.setData({
+      flag: false
+    })
   }
 })
