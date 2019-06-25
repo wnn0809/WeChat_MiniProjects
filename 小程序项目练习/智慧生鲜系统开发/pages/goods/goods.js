7911c8156ac901ac5d9a594093e0807606f97a00
wnn0809 <w1418899532@163.com>
2019-06-25 16:37:32 +0800

    JJ生鲜

--- 小程序项目练习/智慧生鲜系统开发/pages/goods/goods.js
+++ 小程序项目练习/智慧生鲜系统开发/pages/goods/goods.js
@@ -5,119 +5,23 @@ Page({
    */
   data: {
     current: 0,
-    goodslist: [
-      {
-        "id": "001",
-        "name": "泰国龙眼500g*1盒 新品【新人特惠】",
-        "desc": "在你的眼里，有我才甜蜜",
-        "price": "￥9.9",
-        "type": "500g/盒",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560947626478&di=135d4458c99082c95f770c06eb37a943&imgtype=0&src=http%3A%2F%2Fm.360buyimg.com%2Fn12%2Fjfs%2Ft2959%2F131%2F1760517754%2F322551%2F58530361%2F578dc733Nf53c6fc8.jpg%2521q70.jpg"
-      },
-      {
-        "id": "002",
-        "name": "伊利畅轻燕麦+草莓风味乳 酸甜可口",
-        "desc": "这杯酸奶太有料了，要嚼着吃",
-        "price": "￥7.8",
-        "type": "300ml/杯",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560947700643&di=9b5a8df1041dc2e3519bec725f63e7ce&imgtype=0&src=http%3A%2F%2Fpic1.womai.com%2Fupload%2F601%2F604%2F51700%2F84500%2F10481145%2F10481145_1_pic500_7848.jpg"
-      },
-      {
-        "id": "003",
-        "name": "进口弗洛加州香蕉每袋500g起",
-        "desc": "跟鲜花一起运过来的香蕉",
-        "price": "￥6.8",
-        "type": "500g/袋",
-        "pic_url": "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1338648069,3007847363&fm=11&gp=0.jpg"
-      },
-      {
-        "id": "004",
-        "name": "新疆吐鲁番地区哈密瓜1个250g起",
-        "desc": "咬一口吐鲁番风情的甜脆",
-        "price": "￥19.8",
-        "type": "250g/个",
-        "pic_url": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2556288931,3686867963&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "005",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3177330934,2805656105&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "006",
-        "name": "新疆吐鲁番哈密瓜1个250g起",
-        "desc": "咬一口吐鲁番风情的甜脆",
-        "price": "￥19.8",
-        "type": "250g/个",
-        "pic_url": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2556288931,3686867963&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "007",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3177330934,2805656105&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "008",
-        "name": "新疆吐鲁番哈密瓜1个250g起",
-        "desc": "咬一口吐鲁番风情的甜脆",
-        "price": "￥19.8",
-        "type": "250g/个",
-        "pic_url": "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2556288931,3686867963&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "009",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3177330934,2805656105&fm=26&gp=0.jpg"
-      },
-      {
-        "id": "010",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560506564518&di=4f88677f52e8dc56f52c00975828f32a&imgtype=0&src=http%3A%2F%2Fwww.huishi365.com%2Fupload%2Fimage%2F20150822%2F1440227820646638.png"
-      },
-      {
-        "id": "011",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560506351458&di=4b44e7c2d4fcd430475d09bbe8b7ac93&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0178b25b3998e8a80121b994c9706b.jpg%402o.jpg"
-      },
-      {
-        "id": "012",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560506485258&di=6901e75e0731b945d859d59b011d640f&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F5f2236c930433d3ff7544343d965c10a41066afb4d914-H2qf5N_fw658"
-      },
-      {
-        "id": "013",
-        "name": "8424西瓜大个儿，一个5斤起",
-        "desc": "夏季清凉密码=8424",
-        "price": "￥26.9",
-        "type": "500g/个",
-        "pic_url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560506542145&di=2be76514a338e0fded3762a7b0aa7b02&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01792a5723055132f875a399c417fb.jpg"
-      }
-    ]
+    goodslist: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(this.data.goodslist)
+    var self = this
+    wx:wx.request({
+      url: 'https://www.easy-mock.com/mock/5d01eca83066922c5416c8ac/Ananfresh/goodsList',
+      success: function (res) {
+        console.log(res.data.data)
+        self.setData({
+          goodslist: res.data.data
+        })
+      }
+    })
   },
 
   /**
