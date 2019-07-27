e2af07f071d5d7d0afd4b3d4dc1e107c59dd7726
wnn0809 <w1418899532@163.com>
2019-07-27 21:06:19 +0800

    影视小程序

--- 小程序项目练习/影视小程序/miniprogram/pages/comment/comment.js
+++ 小程序项目练习/影视小程序/miniprogram/pages/comment/comment.js
@@ -1,3 +1,5 @@
+// 初始化
+const db = wx.cloud.database();
 Page({
 
   /**
@@ -6,19 +8,105 @@ Page({
   data: {
     detail: {},
     content: '',//评价的内容
-    score: 5
+    score: 5,//默认的评价分数
+    images: [],//上传的图片
+    fileIds: [],
+    movieId: 0,
+    flag:false,
   },
   onContentChange:function(event){
-
+    this.setData({
+      content: event.detail
+    })
   },
   onScoreChange: function (event){
-    
+    this.setData({
+      score: event.detail
+    })
+  },
+  uploadImg :function(){
+    // 上传图片
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success:res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths)
+        this.setData({
+          images: this.data.images.concat(tempFilePaths)
+        })
+      }
+    })
+  },
+  submit: function () {
+    console.log(this.data.content)
+    wx.showToast({
+      title: '评价中',
+    })
+    // 1，上传图片到云存储.2,等云存储返回对应的fileID
+    let promiseArr = [];
+    // 循环遍历每张图片
+    for(let i = 0; i<this.data.images.length;i++){
+      promiseArr.push(new Promise((resolve, reject)=>{
+        let item = this.data.images[i];
+        // 图片后缀
+        //使用正则表达式返回文件的拓展名
+        let suffix=/\.\w+$/.exec(item)[0]; 
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            // 返回文件 ID
+            console.log(res.fileID)
+            this.setData({
+              fileIds: this.data.fileIds.concat(res.fileID)
+            })
+            resolve();
+          },
+          fail: console.error
+        })
+      }))
+    }
+    // 3.将fileID存到对应的云数据库
+    // 等promiseArr数组中每一个promise任务完成后再去执行then里的代码
+    Promise.all(promiseArr).then(res=>{
+      // 插入数据
+      const comment = db.collection('comment').add({
+        data:{
+          content:this.data.content,
+          score:this.data.score,
+          movieid:this.data.movieId,
+          fileIds:this.data.fileIds
+        }
+      }).then(res => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '评价成功',
+        })
+        this.setData({
+          flag: true
+        })
+        }).catch(err => {
+          wx.hideLoading();
+          wx.showToast({
+            title: '评价失败',
+          })
+          this.setData({
+            flag: false
+          })
+        })
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     var movieid = options.movieid.trim()
+    this.setData({
+      movieId: movieid
+    })
     console.log(movieid)
     wx.cloud.callFunction({
       name: 'getdetail',
