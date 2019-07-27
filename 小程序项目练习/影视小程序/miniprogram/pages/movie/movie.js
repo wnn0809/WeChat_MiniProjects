fb69ef1d1423a85fddeb6891676f8bee11d36501
wnn0809 <w1418899532@163.com>
2019-07-27 15:17:19 +0800

    影视小程序

--- 小程序项目练习/影视小程序/miniprogram/pages/movie/movie.js
+++ 小程序项目练习/影视小程序/miniprogram/pages/movie/movie.js
@@ -8,25 +8,39 @@ Page({
     movieList: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  getmovieList: function(){
+    wx.showLoading({
+      title: '加载中',
+    })
     wx.cloud.callFunction({
       // 云函数的名称
-      name:"movielist",
+      name: "movielist",
       // 云函数的参数
       start: this.data.movieList.length,
       //每次请求10条数据
       count: 10
-      }).then(res=>{
-        console.log(res)
-        this.setData({
-          movieList: this.data.movieList.concat(JSON.parse(res.result).subjects)
-        })
-    }).catch(err=>{
+    }).then(res => {
+      console.log(res)
+      this.setData({
+        movieList: this.data.movieList.concat(JSON.parse(res.result).subjects)
+      });
+      wx.hideLoading();
+    }).catch(err => {
       console.error(err)
+      wx.hideLoading();
     })
+  },
+  gotoComment: function(event){
+    // console.log(event.target.dataset.movieid)
+    wx.navigateTo({
+      url: `../comment/comment?movieid=${ event.target.dataset.movieid}`,
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getmovieList();
   },
 
   /**
@@ -61,14 +75,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.getmovieList();
   },
 
   /**
