094f378fb43afd76734f9ab734a4e350815fef41
wnn0809 <w1418899532@163.com>
2019-07-28 11:50:08 +0800

    影视小程序

--- 小程序项目练习/影视小程序/miniprogram/pages/comment/comment.js
+++ 小程序项目练习/影视小程序/miniprogram/pages/comment/comment.js
@@ -1,3 +1,5 @@
+import Notify from '../../miniprogram_npm/vant-weapp/notify/notify';
+
 // 初始化
 const db = wx.cloud.database();
 Page({
@@ -41,49 +43,52 @@ Page({
   },
   submit: function () {
     console.log(this.data.content)
-    wx.showLoading({
-      title: '评价中',
-    })
-    // 1，上传图片到云存储.2,等云存储返回对应的fileID
-    let promiseArr = [];
-    // 循环遍历每张图片
-    for(let i = 0; i<this.data.images.length;i++){
-      promiseArr.push(new Promise((resolve, reject)=>{
-        let item = this.data.images[i];
-        // 图片后缀
-        //使用正则表达式返回文件的拓展名
-        let suffix=/\.\w+$/.exec(item)[0]; 
-        wx.cloud.uploadFile({
-          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
-          filePath: item, // 小程序临时文件路径
-          success: res => {
-            // 返回文件 ID
-            console.log(res.fileID)
-            this.setData({
-              fileIds: this.data.fileIds.concat(res.fileID)
-            })
-            resolve();
-          },
-          fail: console.error
-        })
-      }))
-    }
-    // 3.将fileID存到对应的云数据库
-    // 等promiseArr数组中每一个promise任务完成后再去执行then里的代码
-    Promise.all(promiseArr).then(res=>{
-      // 插入数据
-      const comment = db.collection('comment').add({
-        data:{
-          content:this.data.content,
-          score:this.data.score,
-          movieid:this.data.movieId,
-          fileIds:this.data.fileIds
-        }
-      }).then(res => {
-        wx.hideLoading();
-        wx.navigateTo({
-          url: `../commentnext/commentnext?movieid=${this.data.movieId}`,
-        })
+    if (this.data.content===''){
+      Notify('请输入评价内容');
+    }else{
+      wx.showLoading({
+        title: '评价中',
+      })
+      // 1，上传图片到云存储.2,等云存储返回对应的fileID
+      let promiseArr = [];
+      // 循环遍历每张图片
+      for (let i = 0; i < this.data.images.length; i++) {
+        promiseArr.push(new Promise((resolve, reject) => {
+          let item = this.data.images[i];
+          // 图片后缀
+          //使用正则表达式返回文件的拓展名
+          let suffix = /\.\w+$/.exec(item)[0];
+          wx.cloud.uploadFile({
+            cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+            filePath: item, // 小程序临时文件路径
+            success: res => {
+              // 返回文件 ID
+              console.log(res.fileID)
+              this.setData({
+                fileIds: this.data.fileIds.concat(res.fileID)
+              })
+              resolve();
+            },
+            fail: console.error
+          })
+        }))
+      }
+      // 3.将fileID存到对应的云数据库
+      // 等promiseArr数组中每一个promise任务完成后再去执行then里的代码
+      Promise.all(promiseArr).then(res => {
+        // 插入数据
+        const comment = db.collection('comment').add({
+          data: {
+            content: this.data.content,
+            score: this.data.score,
+            movieid: this.data.movieId,
+            fileIds: this.data.fileIds
+          }
+        }).then(res => {
+          wx.hideLoading();
+          wx.navigateTo({
+            url: `../commentnext/commentnext?movieid=${this.data.movieId}`,
+          })
         }).catch(err => {
           wx.hideLoading();
           wx.showToast({
@@ -93,7 +98,9 @@ Page({
             flag: false
           })
         })
-    })
+      })
+    }
+    
   },
   /**
    * 生命周期函数--监听页面加载
