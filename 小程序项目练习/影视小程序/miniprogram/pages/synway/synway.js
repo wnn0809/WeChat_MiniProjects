b4d3873dfd4ccd133edd7bac7b558cb5dbe4c510
wnn0809 <w1418899532@163.com>
2019-08-23 22:23:10 +0800

    增加个人履历页面

--- 小程序项目练习/影视小程序/miniprogram/pages/synway/synway.js
+++ 小程序项目练习/影视小程序/miniprogram/pages/synway/synway.js
@@ -7,16 +7,43 @@ Page({
     slides: [
       {
         id:"001",
-        image:"../images/banner2.jpg"
+        image:"http://file.xiaomutong.com.cn/2019823nn-banner2.jpg"
       },
       {
         id: "001",
-        image: "../images/banner3.jpg"
+        image: "http://file.xiaomutong.com.cn/2019823nn-banner3.jpg"
       },
       {
         id: "001",
-        image: "../images/banner4.jpg"
+        image: "http://file.xiaomutong.com.cn/2019823nn-banner4.jpg"
       }
+    ],
+    storylist:[
+      {
+        id:"001",
+        image:"http://file.xiaomutong.com.cn/2019823nn-0.png",
+        imgdesc:"见证搬迁史的老工牌"
+      },
+      {
+        id: "002",
+        image: "http://file.xiaomutong.com.cn/2019823nn-3.jpg",
+        imgdesc: "加班餐后-与同事打场羽毛球"
+      },
+      {
+        id: "003",
+        image: "http://file.xiaomutong.com.cn/2019823nn-4.jpg",
+        imgdesc: "2018年7月28-29团建-千岛湖之旅"
+      },
+      {
+        id: "004",
+        image: "http://file.xiaomutong.com.cn/2019823nn-5.jpg",
+        imgdesc: "2019年8月17-18团建-浙江桐庐之旅"
+      },
+      {
+        id: "005",
+        image: "http://file.xiaomutong.com.cn/2019823nn-6.png",
+        imgdesc: "我的第一份work-坚持的道理"
+      },
     ]
   },
 
