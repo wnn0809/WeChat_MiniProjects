18d2dbded52920c84341d79f0363e1074fca7c28
wnn0809 <w1418899532@163.com>
2019-08-24 14:06:42 +0800

    增加个人履历页面

--- 小程序项目练习/影视小程序/miniprogram/pages/filmreview/filmreview.js
+++ 小程序项目练习/影视小程序/miniprogram/pages/filmreview/filmreview.js
@@ -14,7 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
     db.collection('comment').get().then(res => {
       console.log(res.data)
       this.setData({
